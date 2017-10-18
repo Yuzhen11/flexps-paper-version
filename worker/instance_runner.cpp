26e58a457699f6754c293dec24a5e865a9d9f6ce
TatianaJin <tatiana.jin.xy@gmail.com>
2017-10-18 18:10:05 +0800

    [worker|example] auto parallelism tasks & instance runner support

--- worker/instance_runner.cpp
+++ worker/instance_runner.cpp
@@ -5,14 +5,14 @@ namespace husky {
 /*
  * Method to extract local instance
  */
-std::vector<std::pair<int, int>> InstanceRunner::extract_local_instance(const std::shared_ptr<Instance>& instance) const {
+std::vector<std::pair<int, int>> InstanceRunner::extract_local_instance(
+    const std::shared_ptr<Instance>& instance) const {
     auto local_threads = instance->get_threads(worker_info_.get_process_id());
     for (auto& th : local_threads) {
         th.first = worker_info_.global_to_local_id(th.first);
     }
     return local_threads;
 }
-
 
 /*
  * Run the instances
@@ -28,12 +28,36 @@ void InstanceRunner::run_instance(std::shared_ptr<Instance> instance) {
 
     husky::LOG_I << GREEN("[InstanceRunner] Instance id " + std::to_string(instance->get_id()) + " " +
                           std::to_string(local_threads.size()) + "/" + std::to_string(instance->get_num_threads()) +
-                          " current epoch " + std::to_string(instance->get_epoch()) +
-                          " run on process " + std::to_string(worker_info_.get_process_id()));
+                          " current epoch " + std::to_string(instance->get_epoch()) + " run on process " +
+                          std::to_string(worker_info_.get_process_id()));
     bool is_leader = true;  // the first thread in each process is the leader
     for (auto tid_cid : local_threads) {
         // worker threads must not be joinable (must be free)
         assert(units_[tid_cid.first].joinable() == false);
+        assign_worker(tid_cid, instance, is_leader);
+        is_leader = false;  // reset is_leader
+    }
+    std::unordered_set<int> local_threads_set;
+    for (auto tid_cid : local_threads)
+        local_threads_set.insert(tid_cid.first);
+    instance_keeper_.insert({instance->get_id(), std::move(local_threads_set)});
+    // husky::LOG_I << "[InstanceRunner]: instance " + std::to_string(instance.get_id()) + " added";
+}
+
+void InstanceRunner::assign_worker(const std::pair<int, int>& tid_cid, std::shared_ptr<Instance> instance,
+                                   bool is_leader) {
+    if (instance->get_type() == Task::Type::AutoParallelismTaskType) {
+        units_[tid_cid.first] = boost::thread([this, instance, tid_cid, is_leader] {
+            Info info = utility::instance_to_info(*instance, worker_info_, tid_cid, is_leader);
+
+            auto* task = static_cast<AutoParallelismTask*>(task_store_.get_task(instance->get_id()).get());
+            task->get_epoch_lambda()(info, task->get_current_stage_iters());
+            zmq::socket_t socket = cluster_manager_connector_.get_socket_to_recv();
+            zmq_sendmore_int32(&socket, constants::kThreadFinished);
+            zmq_sendmore_int32(&socket, instance->get_id());
+            zmq_send_int32(&socket, tid_cid.first);
+        });
+    } else {
         units_[tid_cid.first] = boost::thread([this, instance, tid_cid, is_leader] {
             // set the info
             Info info = utility::instance_to_info(*instance, worker_info_, tid_cid, is_leader);
@@ -55,13 +79,7 @@ void InstanceRunner::run_instance(std::shared_ptr<Instance> instance) {
             zmq_sendmore_int32(&socket, instance->get_id());
             zmq_send_int32(&socket, tid_cid.first);
         });
-        is_leader = false;  // reset is_leader
     }
-    std::unordered_set<int> local_threads_set;
-    for (auto tid_cid : local_threads)
-        local_threads_set.insert(tid_cid.first);
-    instance_keeper_.insert({instance->get_id(), std::move(local_threads_set)});
-    // husky::LOG_I << "[InstanceRunner]: instance " + std::to_string(instance.get_id()) + " added";
 }
 
 /*
@@ -74,9 +92,7 @@ void InstanceRunner::finish_thread(int instance_id, int tid) {
     units_[tid].join();
 }
 
-bool InstanceRunner::is_instance_done(int instance_id) { 
-    return instance_keeper_[instance_id].empty(); 
-}
+bool InstanceRunner::is_instance_done(int instance_id) { return instance_keeper_[instance_id].empty(); }
 
 void InstanceRunner::remove_instance(int instance_id) {
     assert(instance_keeper_[instance_id].empty());
